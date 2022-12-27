097ac82e8987532c5eaadbf86b444fb0a11ecc5f
msantos9-dev <112072332+msantos9-dev@users.noreply.github.com>
2022-12-27 10:45:35 +0800

    #1 Done with data source config

---
+++ LaundryManagmentSystem/src/main/java/com/global77/lms/LaundryManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.global77.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryManagmentSystemApplication.class, args);
+	}
+
+}
