30c63a2ade6b5106340010b0f4bcdfd2ad95b4d2
msantos9-dev <112072332+msantos9-dev@users.noreply.github.com>
2022-12-27 11:40:39 +0800

    #4 Test customer ctrl

--- LaundryManagmentSystem/src/main/java/com/global77/lms/config/SecurityConfiguration.java
+++ LaundryManagmentSystem/src/main/java/com/global77/lms/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.authorizeRequests().antMatchers("/customer/**").hasRole("CUSTOMER")
 
 				.antMatchers("/page/all", "/registration**", "/js/**",
 						"/css/**", "/img/**")
--- LaundryManagmentSystem/src/main/java/com/global77/lms/contoller/CustomerController.java
+++ LaundryManagmentSystem/src/main/java/com/global77/lms/contoller/CustomerController.java
@@ -1,10 +1,16 @@
 package com.global77.lms.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	@GetMapping("/")
+	public String home() {
+		return "custiner/index";
+	}
 
 }
